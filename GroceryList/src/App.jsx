1421c23d5440a5146bd9e6de066d68fb9055daac
Tanisha <tanisha.engineer2005@gmail.com>
2023-11-15 17:51:19 +0530

    CRUD operations

--- GroceryList/src/App.jsx
+++ GroceryList/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './Footer';
 import Content from './Content';
 import SearchItem from './SearchItem';
 import AddItem from './AddItem';
+import apiRequest from './apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3500/items';
@@ -33,21 +34,50 @@ function App() {
     }, 2000);
   }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method : 'POST',
+      headers : {
+        'Content-type' : 'application/json'
+      },
+      body : JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
+
+    const myItem = listItems.filter(item => item.id === id);
+    const updateOptions = {
+      method : 'PATCH',
+      headers : {
+        'Content-type' : 'application/json'
+      },
+      body : JSON.stringify({ checked : myItem[0].checked })
+    }
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => (item.id !== id));
     setItems(listItems);
+
+    const deleteOption = {
+      method : 'DELETE'
+    } 
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOption);
+    if (result) setFetchError(result);
   }
 
   const handleSubmit = (e) => {
